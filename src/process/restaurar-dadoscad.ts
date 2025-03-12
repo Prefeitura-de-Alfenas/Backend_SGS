ddfadf87281bddc8fba2a7b5a56bb0e72c6a52d8
marceloroot <marcelo.lima.gomes.23@gmail.com>
2025-03-12 17:16:37 -0300

    fix: restauradar dados

--- src/process/restaurar-dadoscad.ts
+++ src/process/restaurar-dadoscad.ts
@@ -1,65 +1,57 @@
 import { PrismaClient } from '@prisma/client';
-import fs from 'fs';
-import csv from 'csv-parser';
-
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
 const prisma = new PrismaClient();
 
+const filePath = '../../mnt/data/DadosCAD.csv';
+
 async function main() {
-  const results: any[] = [];
+  const pessoas = [];
 
-  fs.createReadStream('/mnt/data/DadosCAD - 2024.csv')
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
+  fs.createReadStream(filePath)
+    .pipe(csv({ separator: ';' }))
+    .on('data', (row) => {
+      pessoas.push({
+        id: row.id,
+        nome: row.nome,
+        cpf: row.cpf.replace(/\D/g, ''),
+        sexo: row.sexo,
+        telefone: row.tel || row.tel_cel || null,
+        email: row.email || null,
+        datanascimento: new Date(row.dataNasc.split('/').reverse().join('-')),
+        rg: row.rg || null,
+        escolaridade: 'Não informado', // Adapte conforme necessário
+        estadocivil: row.estadoCivil || 'Não informado',
+        renda: 0, // Definir valor padrão se não houver informação
+        ctpsassinada: parseInt(row.CTPS_Serie) || 0,
+        ppcl: 0,
+        gestante: row.gestante === '1' ? 1 : 0,
+        observacao: null,
+        observacaorestrita: null,
+        cep: '00000-000', // Adapte conforme necessário
+        logradouro: row.endereco || 'Não informado',
+        complemento: row.complemento || null,
+        bairro: row.bairro || 'Não informado',
+        localidade: 'Não informado',
+        numero: row.numero || 'S/N',
+        uf: 'Não informado',
+        status: 'ativo',
+        equipamentoId: '24d5123e-e26e-41cd-8d4f-b07edad680fd', // Precisa ser ajustado conforme necessidade
+      });
+    })
     .on('end', async () => {
-      for (const row of results) {
-        try {
-          console.log('ROW', row);
-          //   await prisma.pessoa.create({
-          //     data: {
-          //       id: row.id,
-          //       nome: row.nome,
-          //       cpf: row.cpf,
-          //       sexo: row.sexo,
-          //       telefone: row.telefone,
-          //       email: row.email,
-          //       datanascimento: new Date(row.datanascimento),
-          //       rg: row.rg,
-          //       parentesco: row.parentesco,
-          //       escolaridade: row.escolaridade,
-          //       estadocivil: row.estadocivil,
-          //       renda: parseFloat(row.renda),
-          //       ctpsassinada: parseInt(row.ctps_assinada),
-          //       ppcl: parseInt(row.ppcl),
-          //       gestante: parseInt(row.gestante),
-          //       observacao: row.observacao,
-          //       observacaorestrita: row.observacaorestrita,
-          //       cep: row.cep,
-          //       logradouro: row.logradouro,
-          //       complemento: row.complemento,
-          //       bairro: row.bairro,
-          //       localidade: row.localidade,
-          //       numero: row.numero,
-          //       uf: row.uf,
-          //       status: row.status,
-          //       equipamentoId: row.equipamentoId,
-          //       pessoaId: row.pessoaId,
-          //   },
-          //  });
-          console.log(`Inserted ${row.nome}`);
-        } catch (error) {
-          console.error(`Error inserting ${row.nome}: `, error);
-        }
+      try {
+        await prisma.pessoa.createMany({ data: pessoas, skipDuplicates: true });
+        console.log('Backup concluído com sucesso!');
+      } catch (error) {
+        console.error('Erro ao inserir dados:', error);
+      } finally {
+        await prisma.$disconnect();
       }
-
-      await prisma.$disconnect();
     });
 }
 
-main()
-  .catch((e) => {
-    console.log('eerror backup ', e);
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+main().catch((error) => {
+  console.error('Erro na execução do script:', error);
+  prisma.$disconnect();
+});
